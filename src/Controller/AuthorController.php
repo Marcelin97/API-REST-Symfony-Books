bbd4afc3b10dbbf9c456cd8b6701787757f0884a
Marcelin97 <lois_m@outlook.com>
2023-10-23 15:24:18 +0200

    :sparkles: Add PHP Doc

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
 use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\Loader\Configurator\serializer;
-use Symfony\Component\HttpFoundation\Response;
 
 class AuthorController extends AbstractController
 {
-    #[Route('/api/author', name: 'author', methods: ['GET'])]
+    /**
+     * Cette méthode permet de récupérer l'ensemble des auteurs
+     *
+     * @param AuthorRepository $authorRepository
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
+    #[Route('/api/authors', name: 'author', methods: ['GET'])]
     public function getAuthorList(AuthorRepository $authorRepository, SerializerInterface $serializer): JsonResponse
     {
         $authorList = $authorRepository->findAll();
@@ -20,10 +28,43 @@ class AuthorController extends AbstractController
         return new JsonResponse($jsonAuthorList, Response::HTTP_OK, [], true);
     }
 
+    /**
+     * Cette méthode permet de récupérer un auteur en particulier en fonction de son id. 
+     *
+     * @param Author $author
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
     #[Route('/api/authors/{id}', name: 'detailAuthor', methods: ['GET'])]
-    public function getDetailAuthor(Author $author, SerializerInterface $serializer)
+    public function getDetailAuthor(Author $author, SerializerInterface $serializer): JsonResponse
     {
         $jsonAuthor = $serializer->serialize($author, 'json', ['groups' => 'getAuthors']);
         return new JsonResponse($jsonAuthor, Response::HTTP_OK, [], true);
+    }
+
+    /**
+     * Cette méthode supprime un auteur en fonction de son id. 
+     * En cascade, les livres associés aux auteurs seront aux aussi supprimés. 
+     *
+     * /!\ Attention /!\
+     * pour éviter le problème :
+     * "1451 Cannot delete or update a parent row: a foreign key constraint fails"
+     * Il faut bien penser rajouter dans l'entité Book, au niveau de l'author :
+     * #[ORM\JoinColumn(onDelete:"CASCADE")]
+     * 
+     * Et resynchronizer la base de données pour appliquer ces modifications. 
+     * avec : php bin/console doctrine:schema:update --force
+     * 
+     * @param Author $author
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
+    #[Route('/api/authors/{id}', name: 'deleteAuthor', methods: ['DELETE'])]
+    public function deleteAuthor(Author $author, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($author);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -8,10 +8,18 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class BookController extends AbstractController
 {
+    /**
+     * Cette méthode permet de récupérer l'ensemble des livres
+     *
+     * @param BookRepository $bookRepository
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
     #[Route('/api/books', name: 'book', methods: ['GET'])]
     public function getBookList(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
     {
@@ -20,10 +28,33 @@ class BookController extends AbstractController
         return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
     }
 
+    
+    /**
+     * Cette méthode permet de récupérer un livre en particulier en fonction de son id. 
+     *
+     * @param SerializerInterface $serializer
+     * @param Book $book
+     * @return JsonResponse
+     */
     #[Route("/api/books/{id}", name: "detailBook", methods: ['GET'])]
-    public function getDetailBook(SerializerInterface $serializer, Book $book)
+    public function getDetailBook(SerializerInterface $serializer, Book $book): JsonResponse
     {
         $jsonBook = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
         return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+    }
+
+    /**
+     * Cette méthode permet de supprimer un livre par rapport à son id. 
+     *
+     * @param Book $book
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
+    #[Route('/api/books/{id}', name: 'deleteBook', methods: ['DELETE'])]
+    public function deleteBook(Book $book, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($book);
+        $em->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
