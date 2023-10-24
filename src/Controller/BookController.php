dde7fab316905c6f3e8cc805a3a7811dc12feaff
Marcelin97 <lois_m@outlook.com>
2023-10-24 11:19:35 +0200

    :sparkles: Create new route to update a book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -12,6 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BookController extends AbstractController
@@ -97,5 +98,32 @@ class BookController extends AbstractController
         $location = $urlGenerator->generate('detailBook', ['id' => $book->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
 
         return new JsonResponse($jsonBook, Response::HTTP_CREATED, ["Location" => $location], true);
+    }
+
+    /**
+     * Cette méthode permet de mettre à jour un livre. 
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param Book $currentBook
+     * @param EntityManagerInterface $em
+     * @param AuthorRepository $authorRepository
+     * @return JsonResponse
+     */#[Route('/api/books/{id}', name: "updateBook", methods: ['PUT'])]
+    public function updateBook(Request $request, SerializerInterface $serializer, Book $currentBook, EntityManagerInterface $em, AuthorRepository $authorRepository): JsonResponse
+    {
+        $updatedBook = $serializer->deserialize(
+            $request->getContent(),
+            Book::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $currentBook]
+        );
+        $content = $request->toArray();
+        $idAuthor = $content['idAuthor'] ?? -1;
+        $updatedBook->setAuthor($authorRepository->find($idAuthor));
+
+        $em->persist($updatedBook);
+        $em->flush();
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 }
